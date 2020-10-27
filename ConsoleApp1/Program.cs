6b02ecc5bdcd965fd4d2cf1cd46b8d678e06fcf8
bluebeacon8402 <crsorge@icloud.com>
2020-10-27 15:46:27 -0400

    new exercise

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         for (int i = 0; i <= 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        
+        
+        
+        }
+    }
+}
